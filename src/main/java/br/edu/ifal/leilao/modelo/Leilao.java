111a4735cc3d75309de7ed6d8a2938c396e138a9
Daphane Leilane <daphaneleilane@hotmail.com>
2019-08-05 09:33:10 -0300

    Primeira alteração do código com email e user, classes e atributos

---
+++ src/main/java/br/edu/ifal/leilao/modelo/Leilao.java
@@ -0,0 +1,29 @@
+package br.edu.ifal.leilao.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leilao {
+
+	private List<Lance> lances;
+	
+	public Leilao(){
+		 lances = new ArrayList<Lance>();
+
+		
+	}
+	
+	public List<Lance> getLances() {
+		return lances;
+	}
+
+	public void setLances(List<Lance> lances) {
+		this.lances = lances;
+	}
+
+
+	
+	
+	
+
+}
